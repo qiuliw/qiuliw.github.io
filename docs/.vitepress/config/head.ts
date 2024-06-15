c11b3fcc92d17c8924d8821ceebec316fc422b4b
qiuliw <1807191473@qq.com>
2024-06-15 20:05:57 +0800

    更新

--- docs/.vitepress/config/head.ts
+++ docs/.vitepress/config/head.ts
@@ -4,7 +4,7 @@ import { metaData } from './constants';
 export const head: HeadConfig[] = [
   ['link', { rel: 'icon', href: '/favicon.ico' }],
   ['meta', { name: 'author', content: 'Charles7c' }],
-  ['meta', { name: 'keywords', content: '查尔斯的知识库, 知识库, 博客, Charles7c' }],
+  ['meta', { name: 'keywords', content: '小邱的知识库, 知识库, 博客, Charles7c' }],
 
   ['meta', { name: 'HandheldFriendly', content: 'True' }],
   ['meta', { name: 'MobileOptimized', content: '320' }],
